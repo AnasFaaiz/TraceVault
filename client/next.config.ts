59860cd6d850671bac15c907e67b4efef6955ac7
AnasFaaiz <syed.anasfaaiz@gmail.com>
2026-03-30 00:12:20 +0530

    feat: Add Vercel rewrites for API proxy

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://23.20.129.69/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
